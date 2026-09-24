db02ae44f739890c7b17f2e97c41597975b90019
abhisheksth9 <abhisheksth7@gmail.com>
2026-09-24 15:14:40 +0545

    fix uuid error

--- services/auth-service/models/user.js
+++ services/auth-service/models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Model } = require('sequelize');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
@@ -9,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     User.init({
-        uuid:     { type: DataTypes.STRING, allowNull: false, unique:true},
+        uuid:     { type: DataTypes.STRING, allowNull: false, unique:true, defaultValue: () => uuidv4() },
         name:     { type: DataTypes.STRING, allowNull: false },
         email:    { type: DataTypes.STRING, allowNull: false, unique: true },
         password: { type: DataTypes.STRING, allowNull: false },
